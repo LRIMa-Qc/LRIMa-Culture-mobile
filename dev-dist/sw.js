85a1e2156a3cce4fa8e5fb917126b5cd8150493e
Samy Rahmani <samy.osmium@gmail.com>
2025-01-15 12:34:57 -0500

    idk

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.njknb8ijq9"
+    "revision": "0.clp0q06rvm8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
